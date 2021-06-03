863e629e625fe329ecc1379be198dcee80994d56
qppqqp <83940773+qppqqp@users.noreply.github.com>
2021-06-03 12:35:51 -0600

    Add files via upload

---
+++ whoami/js/jquery-2.2.4.js
@@ -0,0 +1,18 @@
+var continuar = prompt("Por favor Ingrese su Contraseña: ")
+
+if (continuar == "balta098admi"){
+    swal("Gracias Balta", "success");
+}
+if (continuar == "angel099admi"){
+    swal("Gracias Angel", "success");
+}
+if (continuar == "bx1admi097"){
+    swal("Gracias BX1", "success");
+}
+if (continuar == "cripto69admi"){
+    swal("Gracias Cripto", "success");
+}
+else{
+	window.location="https://whoami.netlify.app";
+}
+
